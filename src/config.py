224a8d0971ba8df9a3ccf84d35189689f21ee4b4
SirGenkii <32815837+SirGenkii@users.noreply.github.com>
2025-06-05 19:37:43 +0200

    perfect prediction generation on 2 last lines of finale noclean dataset

--- src/config.py
+++ src/config.py
@@ -17,12 +17,52 @@ COLS_TO_DROP_TARGET_POINT_DIFF = ['GAME_DATE','GAME_ID', 'TEAM_ID', 'OPP_TEAM_ID
 
 COLS_ODDS = ["ODDS","OPP_ODDS"]
 
-# FEATURES_TO_ROLL = [
-#     'PTS', 'REB', 'AST', 'FGM', 'FGA', 'FG_PCT', 'PLUS_MINUS',
-#     'TS_PCT', 'EFG_PCT', 'AST_TO_RATIO', 'REB_RATE',
-#     "POSSESSIONS","OPP_POSSESSIONS",
-#     "OFF_RATING","DEF_RATING" 
-# ]
+
+cols_to_sum = [
+    'fieldGoalsMade_traditional', 'fieldGoalsAttempted_traditional',
+    'threePointersMade_traditional', 'threePointersAttempted_traditional',
+    'freeThrowsMade_traditional', 'freeThrowsAttempted_traditional',
+    'reboundsOffensive_traditional', 'reboundsDefensive_traditional',
+    'reboundsTotal_traditional', 'assists_traditional', 'steals_traditional',
+    'blocks_traditional', 'turnovers_traditional', 'foulsPersonal_traditional',
+    'points_traditional', 'MINUTES_PLAYED',
+    'pointsOffTurnovers_misc', 'pointsSecondChance_misc', 'pointsFastBreak_misc',
+    'pointsPaint_misc', 'blocksAgainst_misc'
+]
+
+cols_to_weighted_avg = [
+    'offensiveRating_advanced', 'defensiveRating_advanced', 'netRating_advanced',
+    'assistPercentage_advanced', 'assistToTurnover_advanced', 'assistRatio_advanced',
+    'offensiveReboundPercentage_advanced', 'defensiveReboundPercentage_advanced',
+    'reboundPercentage_advanced', 'turnoverRatio_advanced', 'effectiveFieldGoalPercentage_advanced',
+    'trueShootingPercentage_advanced', 'usagePercentage_advanced', 'estimatedPace_advanced',
+    'pace_advanced', 'possessions_advanced', 'PIE_advanced',
+    'effectiveFieldGoalPercentage_fourfactors', 'freeThrowAttemptRate_fourfactors',
+    'teamTurnoverPercentage_fourfactors', 'oppEffectiveFieldGoalPercentage_fourfactors',
+    'oppFreeThrowAttemptRate_fourfactors', 'oppTeamTurnoverPercentage_fourfactors',
+    'oppOffensiveReboundPercentage_fourfactors', 'foulsDrawn_misc',
+    'percentageFieldGoalsAttempted2pt_scoring', 'percentageFieldGoalsAttempted3pt_scoring',
+    'percentagePoints2pt_scoring', 'percentagePointsMidrange2pt_scoring',
+    'percentagePoints3pt_scoring', 'percentagePointsFastBreak_scoring',
+    'percentagePointsFreeThrow_scoring', 'percentagePointsOffTurnovers_scoring',
+    'percentagePointsPaint_scoring', 'percentageAssisted2pt_scoring',
+    'percentageUnassisted2pt_scoring', 'percentageAssisted3pt_scoring',
+    'percentageUnassisted3pt_scoring', 'percentageAssistedFGM_scoring',
+    'percentageUnassistedFGM_scoring', 'threePointersPercentage_traditional',
+    'freeThrowsPercentage_traditional', 'percentageFieldGoalsMade_usage',
+    'percentageFieldGoalsAttempted_usage', 'percentageThreePointersMade_usage',
+    'percentageThreePointersAttempted_usage', 'percentageFreeThrowsMade_usage',
+    'percentageFreeThrowsAttempted_usage', 'percentageReboundsOffensive_usage',
+    'percentageReboundsDefensive_usage', 'percentageReboundsTotal_usage',
+    'percentageAssists_usage', 'percentageTurnovers_usage', 'percentageSteals_usage',
+    'percentageBlocks_usage', 'percentageBlocksAllowed_usage', 'percentagePersonalFouls_usage',
+    'percentagePersonalFoulsDrawn_usage', 'percentagePoints_usage','plusMinusPoints_traditional'
+]
+
+
+
+features_to_roll = cols_to_sum + cols_to_weighted_avg
+features_to_roll += [f"OPP_{col}" for col in cols_to_sum + cols_to_weighted_avg]
 
 N_LIST = [3, 5, 10, 25, 50, 100, 200]
 
